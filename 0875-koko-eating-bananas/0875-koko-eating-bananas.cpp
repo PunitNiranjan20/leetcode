e94f99085e14f7f8fd4f2a4c1fda4a4b2b470b46
PunitNiranjan <152775594+PunitNiranjan20@users.noreply.github.com>
2026-06-27 21:01:57 +0530

    Time: 13 ms (22.78%), Space: 39.5 MB (5.02%) - LeetHub

---
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    long long countv(vector<int>arr,int x){
+        long long count = 0;
+        for(int i=0;i<arr.size();i++){
+            // count += arr[i]/x;
+            // if(arr[i]%x != 0) count++;
+            int hours = ceil((double)arr[i]/x);
+            count+= hours;
+        }
+        return count;
+    }
+    int minEatingSpeed(vector<int>& piles, int h) {
+        int r = *max_element(piles.begin(),piles.end());
+        // for(auto pile : piles){
+        // if(pile>r) r = pile;
+        // }
+        int l = 1;
+        int ans;
+        while(l<=r){
+            int mid = l+(r-l)/2;
+            long long count = countv(piles,mid);
+            if(count<=h){
+                ans = mid;
+                r = mid-1;
+            }else{
+                l = mid+1; 
+            }
+        }
+        return ans;
+    }
+};
