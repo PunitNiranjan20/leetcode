b3de8c4bbca74bab93f493234e99157a11a74d33
PunitNiranjan <152775594+PunitNiranjan20@users.noreply.github.com>
2026-06-27 21:03:13 +0530

    Time: 17 ms (11.99%), Space: 39.6 MB (5.02%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -3,18 +3,19 @@ public:
     long long countv(vector<int>arr,int x){
         long long count = 0;
         for(int i=0;i<arr.size();i++){
-            // count += arr[i]/x;
-            // if(arr[i]%x != 0) count++;
-            int hours = ceil((double)arr[i]/x);
-            count+= hours;
+            count += arr[i]/x;
+            if(arr[i]%x != 0) count++;
+            // int hours = ceil((double)arr[i]/x);
+            // count+= hours;
         }
         return count;
     }
     int minEatingSpeed(vector<int>& piles, int h) {
-        int r = *max_element(piles.begin(),piles.end());
-        // for(auto pile : piles){
-        // if(pile>r) r = pile;
-        // }
+        // int r = *max_element(piles.begin(),piles.end());
+        int r =0;
+        for(auto pile : piles){
+        if(pile>r) r = pile;
+        }
         int l = 1;
         int ans;
         while(l<=r){
