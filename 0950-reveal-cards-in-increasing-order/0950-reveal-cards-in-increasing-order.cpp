16a11a9bdceb4d4aad0937ae38b1aa0b8c743df9
PunitNiranjan <152775594+PunitNiranjan20@users.noreply.github.com>
2026-07-10 12:02:22 +0530

    Time: 0 ms (100%), Space: 12.6 MB (52.39%) - LeetHub

--- 0950-reveal-cards-in-increasing-order/0950-reveal-cards-in-increasing-order.cpp
+++ 0950-reveal-cards-in-increasing-order/0950-reveal-cards-in-increasing-order.cpp
@@ -6,11 +6,16 @@ public:
         int ind = 0;
         vector<int>ans(desk.size());
         for(int i=0;i<desk.size();i++) q.push(i);
-        while(!q.empty()){
+        // while(!q.empty()){
+        for(int i=0;i<desk.size();i++)
+        {
             ans[q.front()] = desk[ind++];
             q.pop();
+            if(!q.empty())
+            {
             q.push(q.front());
             q.pop();
+            }
         }
         return ans;
     }
