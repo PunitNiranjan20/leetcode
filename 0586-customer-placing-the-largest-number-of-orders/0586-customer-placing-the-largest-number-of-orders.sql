d43e2669a806fc122bba9e4c3995ee2a775fdc94
PunitNiranjan <152775594+PunitNiranjan20@users.noreply.github.com>
2026-07-20 12:39:30 +0530

    Time: 454 ms (84.16%), Space: 0B (100%) - LeetHub

--- 0586-customer-placing-the-largest-number-of-orders/0586-customer-placing-the-largest-number-of-orders.sql
+++ 0586-customer-placing-the-largest-number-of-orders/0586-customer-placing-the-largest-number-of-orders.sql
@@ -1,1 +1,1 @@
-select customer_number as customer_number from (select customer_number,count(*)as count  from orders group by customer_number order by count desc limit 1) as subquery;
+select customer_number from (select customer_number, count(*) as count from orders group by customer_number order by count desc limit 1) as a;
