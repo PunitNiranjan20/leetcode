65a73c47cca7756247900b87d8651f9596d7a427
PunitNiranjan <152775594+PunitNiranjan20@users.noreply.github.com>
2026-07-30 12:24:01 +0530

    Time: 115 ms (5.14%), Space: 27.6 MB (65.1%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.cpp
+++ 0695-max-area-of-island/0695-max-area-of-island.cpp
@@ -1,20 +1,19 @@
 class Solution {
 public:
     int ans = 0;
-   
-    void  dfs(vector<vector<int>>&grid,int i,int j,int &count){
+    int count = 0;
+    void  dfs(vector<vector<int>>&grid,int i,int j){
         if(i<0||j<0||i>=grid.size()||j>=grid[i].size()||grid[i][j]==0){
              return;
         }
-        
         grid[i][j] = 0;
        count++;
-       
+       ans = max(count,ans);
         cout<<ans<<" "<<count<<endl;
-        dfs(grid,i+1,j,count);
-       dfs(grid,i-1,j,count);
-      dfs(grid,i,j+1,count);
-       dfs(grid,i,j-1,count);
+        dfs(grid,i+1,j);
+       dfs(grid,i-1,j);
+      dfs(grid,i,j+1);
+       dfs(grid,i,j-1);
    
         
     }
@@ -22,9 +21,8 @@ public:
         for(int i=0;i<grid.size();i++){
             for(int j = 0;j<grid[i].size();j++){
                 if(grid[i][j]==1){
-                    int count=0;
-                  dfs(grid,i,j,count);
-                  ans = max(ans ,count);
+                  dfs(grid,i,j);
+                  count = 0;
                     cout<<endl;
                 }
             }
